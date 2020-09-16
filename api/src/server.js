dead4766a7d61d3fa87583be5da1d55dd5e51334
Diogenes <diogensgreen@gmail.com>
2020-09-16 08:08:54 -0300

    adicionando setup mongodb e dotenv

--- api/src/server.js
+++ api/src/server.js
@@ -1,11 +1,26 @@
 const express = require('express')
+require('dotenv').config()
 const path = require("path");
 var hbs = require('express-handlebars');
 const bodyParser = require('body-parser')
+
+const ObjectId = require('mongodb').ObjectID
+
+const MongoClient = require('mongodb').MongoClient
+const uri = `mongodb+srv://${process.env.USER}:${process.env.SENHA}@cluster0.awox0.mongodb.net/${process.env.BANCO}?retryWrites=true&w=majority`;
+
 const app = express()
 
-const PORT = 8080
+const PORT = process.env.PORT || 3333
 
+MongoClient.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true},(err, client) => {
+  if (err) return console.log(err)
+  db = client.db('CRUD')
+  
+  app.listen(PORT, () => {
+    console.log(`🔥 Start -> http://localhost:${PORT}/`)
+  }, )
+}, )
 
 app.use(express.json())
 app.use(bodyParser.urlencoded({
@@ -25,6 +40,6 @@ app.set("views", path.join(__dirname, "views"));
 
 app.use("/api", require("./routes/index"));
 
-app.listen(PORT, () => {
-    console.log(`🔥 Start -> http://localhost:${PORT}/`)
-})
+
+
+
